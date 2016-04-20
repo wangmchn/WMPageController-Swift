e8b0e94efa1d76c7ca2cc09487511f6f0426de7b
wangmchn <wangmchn@163.com>
2016-04-20 21:34:55 +0800

    Create framework

---
+++ WMPageControllerSwiftFramework/WMPageControllerSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  WMPageControllerSwiftFramework.h
+//  WMPageControllerSwiftFramework
+//
+//  Created by Mark on 16/4/20.
+//  Copyright © 2016年 Wecan Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WMPageControllerSwiftFramework.
+FOUNDATION_EXPORT double WMPageControllerSwiftFrameworkVersionNumber;
+
+//! Project version string for WMPageControllerSwiftFramework.
+FOUNDATION_EXPORT const unsigned char WMPageControllerSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WMPageControllerSwiftFramework/PublicHeader.h>
+
+
